713f8559ca10c0bd17765aadf68c5470a81640e3
Андрей Школин <andrey.shkolin@lenta.com>
2026-09-08 23:29:53 +0300

    Удалён неиспользуемый код

--- src/main/java/ru/yandex/practicum/blog/model/Comment.java
+++ src/main/java/ru/yandex/practicum/blog/model/Comment.java
@@ -16,23 +16,11 @@ public class Comment {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getPostId() {
         return postId;
     }
 
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
     public String getText() {
         return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 }
--- src/main/java/ru/yandex/practicum/blog/model/Post.java
+++ src/main/java/ru/yandex/practicum/blog/model/Post.java
@@ -18,31 +18,15 @@ public class Post {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public int getLikesCount() {
         return likesCount;
-    }
-
-    public void setLikesCount(int likesCount) {
-        this.likesCount = likesCount;
     }
 }
